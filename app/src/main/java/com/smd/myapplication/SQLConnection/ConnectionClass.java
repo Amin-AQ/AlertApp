3028c54c43a10f71dc79278744e41d419be39205
Amin A <aminqasmi78@gmail.com>
2023-04-04 14:21:50 +0500

    Initial commit

---
+++ app/src/main/java/com/smd/myapplication/SQLConnection/ConnectionClass.java
@@ -0,0 +1,9 @@
+package com.smd.myapplication.SQLConnection;
+
+public class ConnectionClass {
+    public static String ip="192.168.1.130";
+    public static String port="49342";
+    public static String username="sa";
+    public static String password="12345678";
+    public static String db="logs";
+}
